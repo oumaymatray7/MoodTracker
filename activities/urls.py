4ac02a65a82a2c39a561950a2c9490076cb80f46
oumaymatray7 <oumaymatray7@gmail.com>
2025-04-19 23:55:28 +0100

    modifier continu

--- activities/urls.py
+++ activities/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.accueil_public, name='accueil'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('dashboard/liste/', views.liste_activites, name='liste'),
+    path('', views.accueil_public, name='accueil_public'),   # page d'accueil publique
+    path('dashboard/', views.dashboard, name='dashboard'),   # tableau de bord
+    path('dashboard/liste/', views.liste_activites, name='liste'),  # liste des activités
+    path('send-feedback/', views.send_feedback, name='send_feedback'),  # Formulaire de contact
 ]
--- activities/views.py
+++ activities/views.py
@@ -1,5 +1,9 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 from activities.models import ActiviteBienEtre
 
@@ -14,3 +18,20 @@ def dashboard(request):
 @login_required
 def liste_activites(request):
     return render(request, 'dashboard/liste.html')
+
+
+
+@login_required  # facultatif pour ce type de formulaire
+def send_feedback(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        msg_type = request.POST.get('type')
+        message = request.POST.get('message')
+
+        print("Feedback reçu :", name, email, msg_type, message)
+
+        messages.success(request, "Merci ! Votre message a été envoyé.")
+        return HttpResponseRedirect(reverse('accueil_public'))
+
+    return HttpResponseRedirect(reverse('accueil_public'))
